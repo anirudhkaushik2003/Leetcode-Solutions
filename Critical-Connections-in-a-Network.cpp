08871cce6c4adee1c185ab3d296483db9e6d5396
Anirudh Kaushik <anirudh.ka@research.iiit.ac.in>
2023-06-14 20:32:06 +0530

    tarjan bridges

--- Critical-Connections-in-a-Network.cpp
+++ Critical-Connections-in-a-Network.cpp
@@ -15,113 +15,53 @@ using namespace std;
 class Solution
 {
 public:
-    void make_set(ll v, vll &parent, vll &rank)
+    void dfsBR(int u, int p, vi &low, vi &disc, vector<vi> &adj, int Time, vector<vi> &br)
     {
-        parent[v] = v;
-        rank[v] = 0;
-    }
-
-    ll find_set(ll v, vll &parent)
-    {
-        if (v == parent[v])
-            return v;
-        return parent[v] = find_set(parent[v], parent);
-    }
-
-    void union_sets(ll a, ll b, vll &parent, vll &rank)
-    {
-        a = find_set(a, parent);
-        b = find_set(b, parent);
-        if (a != b)
+        low[u] = disc[u] = ++Time;
+        for (int &v : adj[u])
         {
-            if (rank[a] < rank[b])
-                swap(a, b);
-            parent[b] = a;
-            if (rank[a] == rank[b])
-                rank[a]++;
+            if (v == p)
+                continue; // we don't want to go back through the same path.
+                          // if we go back is because we found another way back
+            if (!disc[v])
+            {                                      // if V has not been discovered before
+                dfsBR(v, u, low, disc, adj, Time, br); // recursive DFS call
+                if (disc[u] < low[v])              // condition to find a bridge
+                {
+                    br.push_back({});
+                    br.back().push_back(u);
+                    br.back().push_back(v);
+                }
+                low[u] = min(low[u], low[v]); // low[v] might be an ancestor of u
+            }
+            else                               // if v was already discovered means that we found an ancestor
+                low[u] = min(low[u], disc[v]); // finds the ancestor with the least discovery time
         }
     }
+
     vector<vector<int>> criticalConnections(int n, vector<vector<int>> &connections)
     {
 
         vector<vi> graph(n, vi());
-        vll parent(n);
-        vll rank(n);
-        for (auto it : connections)
-        {
-            graph[it[0]].pb(it[1]);
-            graph[it[1]].pb(it[0]);
-        }
-        for (int i = 0; i < n; i++)
-        {
-            make_set(i, parent, rank);
-        }
-        vector<vi> minimum_spanning_tree(n, vi()); // edges for each  vertex in minimum spanning tree
-        for (auto it : connections)
-        {
-            if (find_set(it[0], parent) != find_set(it[1], parent))
-            {
-                union_sets(it[0], it[1], parent, rank);
-                minimum_spanning_tree[it[0]].pb(it[1]);
-                minimum_spanning_tree[it[1]].pb(it[0]);
-            }
-        }
+        vector<vll> cycle_edges;
         vector<vi> critical_connections;
-        vector<vi> new_graph(n, vi());
-        // remove all vertices from the graph which belong to minimum spanning tree
-        for (int i = 0; i < n; i++)
+        for (int i = 0; i < connections.size(); i++)
         {
-            sort(minimum_spanning_tree[i].begin(), minimum_spanning_tree[i].end());
-            sort(graph[i].begin(), graph[i].end());
-            // remove all indices in the graph which are present in the minimum spanning tree
+            if (connections[i][0] > connections[i][1])
+            {
+                swap(connections[i][0], connections[i][1]);
+            }
 
-            int j = 0;
-            int k = 0;
-            for (j = 0; j < minimum_spanning_tree[i].size();)
-            {
-                if (minimum_spanning_tree[i][j] == graph[i][k])
-                {
-                    j++;
-                    k++;
-                }
-                else if (minimum_spanning_tree[i][j] > graph[i][k])
-                {
-                    new_graph[i].pb(graph[i][k]);
-                    new_graph[graph[i][k]].pb(i);
-                    cout <<"edge: " << i << " " << graph[i][k] << endl;
-                    k++;
-                }
-            }
-            while (k < graph[i].size())
-            {
-                new_graph[i].pb(graph[i][k]);
-                new_graph[graph[i][k]].pb(i);
-                k++;
-            }
+            graph[connections[i][0]].pb(connections[i][1]);
+            graph[connections[i][1]].pb(connections[i][0]);
         }
-        vector<bool> visited(n, false);
-        vector<bool> avoid(n, false);
-        for (int i = 0; i < n; i++)
-        {
-            if (new_graph[i].size() != 0)
-                avoid[i] = true;
-        }
-        for (int i = 0; i < n; i++)
-        {
-            if (new_graph[i].size() == 0)
-            {
-                if (visited[i])
-                    continue;
-                visited[i] = true;
-                for (auto it : minimum_spanning_tree[i])
-                {
-                    if (visited[it] || avoid[it])
-                        continue;
-                    critical_connections.pb({i, it});
-                    visited[it] = true;
-                }
-            }
-        }
+        // find bridges
+        vi low(n, 0), disc(n, 0);
+        int Time = 0;
+        for (int u = 0; u < n; u++)
+            if (!disc[u])
+                dfsBR(u, u, low, disc, graph, Time, critical_connections);
+
         return critical_connections;
     }
 };
@@ -132,12 +72,12 @@ int main()
     // connections = [[0,1],[1,2],[2,0],[1,3],[3,4],[4,5],[5,3]]
     vector<vi> connections = {{0, 1}, {1, 2}, {2, 0}, {1, 3}, {3, 4}, {4, 5}, {5, 3}};
     vector<vi> critical_connections = s.criticalConnections(6, connections);
-    for (auto connection : critical_connections)
-    {
-        for (auto node : connection)
+        for (auto connection : critical_connections)
         {
-            cout << node << " ";
+            for (auto node : connection)
+            {
+                cout << node << " ";
+            }
+            cout << endl;
         }
-        cout << endl;
-    }
 }
