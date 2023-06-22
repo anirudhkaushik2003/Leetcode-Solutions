dddc19bb72c006131c2f7441a94cc58748134819
Anirudh Kaushik <anirudh.ka@research.iiit.ac.in>
2023-06-22 14:59:42 +0530

    beats 94.6%, io syncing + unordered map

--- Longest-Arithmetic-Subsequence-of-Given-Difference.cpp
+++ Longest-Arithmetic-Subsequence-of-Given-Difference.cpp
@@ -8,97 +8,27 @@ using namespace std;
 #define pll pair<ll, ll>
 #define pii pair<int, int>
 #define vi vector<int>
-
+static auto _ = []()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return nullptr;
+}();
 class Solution
 {
 public:
-    int binarySearch(vector<pii> arr, int l, int r, int target)
-    {
-        int mid;
-        while (l <= r)
-        {
-            mid = (l + r) >> 1;
-            if (arr[mid].first == target)
-            {
-                return mid;
-            }
-            if (arr[mid].first < target)
-            {
-                l = mid + 1;
-            }
-            else
-            {
-                r = mid - 1;
-            }
-        }
-        return -1;
-    }
     int longestSubsequence(vector<int> &arr, int difference)
     {
         int n = arr.size();
-        map<int, vector<int>> m;
-        vi dp(n, 1);
-        vector<bool> visited(n, false);
-
-        for (int i = 0; i < n; i++)
+        unordered_map<int, int> dp;
+        int max_so_far = 1;
+        for (auto it : arr)
         {
-            if (m.find(arr[i]) == m.end())
-            {
-                m[arr[i]] = vi();
-                m[arr[i]].push_back(i);
-            }
-            else
-            {
-                m[arr[i]].push_back(i);
-            }
+            dp[it] = dp.count(it - difference) ? dp[it - difference] + 1 : 1;
+            max_so_far = max(max_so_far, dp[it]);
         }
-
-        for(auto it :m)
-        {
-            sort(m[it.first].begin(), m[it.first].end());
-        }
-        int cur_max = 0;
-        int cur, req, ind, cond, j;
-
-        for (int i = 0; i < n; i++)
-        {
-            if (!visited[i])
-            {
-                // cout << arr[i] << " ";
-                visited[i] = true;
-                cur = arr[i];
-                req = arr[i] + difference;
-                ind = i;
-                // it = binarySearch(sorted, 0, n - 1, req);
-                while (m.find(req) != m.end() )
-                {
-                    cur = req;
-                    cond = 0;
-                    for(auto it: m[req])
-                    {
-                        if(it > ind)
-                        {
-                            ind = it;
-                            cond = 1;
-                            break;
-                        }
-                    }
-                    if(cond == 0)
-                    {
-                        break;
-                    }
-                    // cout << cur << " ";
-                    // ind = sorted[it].second;
-                    visited[ind] = true;
-                    req = cur + difference;
-                    dp[i]++;
-                    // it = binarySearch(sorted, 0, n - 1, req);
-                }
-                // cout << endl;
-                cur_max = max(cur_max, dp[i]);
-            }
-        }
-        return cur_max;
+        return max_so_far;
     }
 };
 
