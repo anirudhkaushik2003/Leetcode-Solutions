1bbf0ba62fa82409c8cae91195dbb9e6b8b198ec
Anirudh Kaushik <anirudh.ka@research.iiit.ac.in>
2023-07-08 14:01:16 +0530

    Combination sum, recursive solution

--- CombinationSum4.cpp
+++ CombinationSum4.cpp
@@ -8,36 +8,43 @@ using namespace std;
 #define pll pair<ll, ll>
 #define pii pair<int, int>
 #define vi vector<int>
-static auto _ = []()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    return nullptr;
-}();
+// static auto _ = []()
+// {
+//     ios::sync_with_stdio(false);
+//     cin.tie(nullptr);
+//     cout.tie(nullptr);
+//     return nullptr;
+// }();
 
 class Solution
 {
 public:
+    int combinationSum4sub(vector<int> &nums, int target, vector<unsigned int> &dp)
+    {
+        if (target == 0)
+        {
+            return 1;
+        }
+        if (dp[target] != -1)
+        {
+            return dp[target];
+        }
+        int ans = 0;
+        for (auto num : nums)
+        {
+            if ( target >= num)
+                ans += combinationSum4sub(nums, target - num, dp);
+        }
+        dp[target] = ans;
+        return dp[target];
+    }
     int combinationSum4(vector<int> &nums, int target)
     {
         sort(nums.begin(), nums.end());
-        int n = nums.size();
-        vector<unsigned int> dp(target + 1, 0);
+        vector<unsigned int> dp(target + 1, -1);
         dp[0] = 1;
 
-        for (int i = 1; i <= target; i++)
-        {
-            for (auto num : nums)
-            {
-                if (num > i)
-                    break;
-
-                dp[i] += dp[i - num];
-            }
-        }
-
-        return dp[target];
+        return combinationSum4sub(nums, target, dp);
     }
 };
 
