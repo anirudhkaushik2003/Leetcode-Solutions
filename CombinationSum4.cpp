4043ec09ca6dc2a6b683eb8cec87e096c2b20069
Anirudh Kaushik <anirudh.ka@research.iiit.ac.in>
2023-07-08 14:02:10 +0530

    Combination sum 4, iterative solution

--- CombinationSum4.cpp
+++ CombinationSum4.cpp
@@ -19,32 +19,25 @@ using namespace std;
 class Solution
 {
 public:
-    int combinationSum4sub(vector<int> &nums, int target, vector<unsigned int> &dp)
-    {
-        if (target == 0)
-        {
-            return 1;
-        }
-        if (dp[target] != -1)
-        {
-            return dp[target];
-        }
-        int ans = 0;
-        for (auto num : nums)
-        {
-            if ( target >= num)
-                ans += combinationSum4sub(nums, target - num, dp);
-        }
-        dp[target] = ans;
-        return dp[target];
-    }
     int combinationSum4(vector<int> &nums, int target)
     {
         sort(nums.begin(), nums.end());
-        vector<unsigned int> dp(target + 1, -1);
+        int n = nums.size();
+        vector<unsigned int> dp(target + 1, 0);
         dp[0] = 1;
 
-        return combinationSum4sub(nums, target, dp);
+        for (int i = 1; i <= target; i++)
+        {
+            for (auto num : nums)
+            {
+                if (num > i)
+                    break;
+
+                dp[i] += dp[i - num];
+            }
+        }
+
+        return dp[target];
     }
 };
 
